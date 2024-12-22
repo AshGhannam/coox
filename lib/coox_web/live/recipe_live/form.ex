0c65dd2b65a7e8bd4712a472453a6ab1812b5db3
Ashraf ghannam <ghannam80@gmail.com>
2024-12-22 11:08:03 +0300

    preserve textarea height with a hook

--- lib/coox_web/live/recipe_live/form.ex
+++ lib/coox_web/live/recipe_live/form.ex
@@ -10,7 +10,7 @@ defmodule CooxWeb.RecipeLive.Form do
 
     <.simple_form for={@form} id="recipe-form" phx-change="validate" phx-submit="save">
       <.input field={@form[:name]} type="text" label="Name" phx-debounce="2000"/>
-      <.input field={@form[:description]} type="textarea" label="Description" phx-debounce="2000" />
+      <.input field={@form[:description]} type="textarea" label="Description" phx-debounce="2000" phx-hook="MaintainHeight" />
 
       <:actions>
         <div class="flex">
